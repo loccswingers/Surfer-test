afae9f22ff5a092c3a4d08b7f9169d9881d4434b
loccswingers <144750641+loccswingers@users.noreply.github.com>
2023-10-18 15:27:00 -0500

    te

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("./uv/uv.bundle.js");
-importScripts("./uv/uv.config.js");
-importScripts("./uv/uv.sw.js");
+importScripts("/contact/mathematics.js");
+importScripts("/contact/geography.js");
+importScripts("/contact/english.js");
 
 const sw = new UVServiceWorker();
 let userKey = new URL(location).searchParams.get('userkey');
