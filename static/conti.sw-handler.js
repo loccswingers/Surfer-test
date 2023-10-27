c6c4932f7e5b6635fb434be51444c5d55c5bece4
loccswingers <144750641+loccswingers@users.noreply.github.com>
2023-10-27 16:58:57 -0500

    remove

--- static/conti.sw-handler.js
+++
@@ -1,9 +0,0 @@
-importScripts('/conti/conti.bundle.js');
-importScripts('/conti/conti.config.js');
-importScripts(__uv$config.sw || '/conti/conti.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', event => {
-    event.respondWith(sw.fetch(event))
-});
